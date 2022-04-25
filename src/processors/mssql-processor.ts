aaf30f245e7fe53bf0c74ef864d7bd7f47ee9cfb
Jans Ivans Jevdokimovs <jans.ivans@gmail.com>
2022-04-25 21:06:39 +0300

    Fix MSSQL processor emitting null row when no rows are processed.

--- src/processors/mssql-processor.ts
+++ src/processors/mssql-processor.ts
@@ -1,5 +1,3 @@
-import { Readable } from 'stream';
-
 import { IMssqlProcessorConfig, processStream } from '../types';
 import { getDb } from '../utils';
 import { DatabaseProcessor } from './base/database-processor';
@@ -11,61 +9,11 @@ export class MssqlProcessor extends DatabaseProcessor {
     }
 
     async process(processStream: processStream) {
-        let done = false;
-        const readStream = new Readable({
-            objectMode: true,
-            async read() {
-                if (data.length === 0) {
-                    if (!done) {
-                        try {
-                            await processRows();
-                        } catch (err) {
-                            this.emit('error', err);
-                        }
-                    } else {
-                        return this.push(null);
-                    }
-                }
-                const rowData = data.shift();
-                this.push(rowData);
-            },
-        });
-        const data: any[] = [];
         const db = await getDb(this.connection, 'mssql');
         const dbRequest = db.request();
-        dbRequest.stream = true;
+        const readStream = dbRequest.toReadableStream();
         dbRequest.query(this.query);
-        const processRows = async () => {
-            return new Promise<void>(async (resolve, reject) => {
-                function removeListeners() {
-                    dbRequest.removeListener('row', onRow);
-                    dbRequest.removeListener('done', onDone);
-                    dbRequest.removeListener('error', onError);
-                };
-                async function onRow(row: any) {
-                    data.push(row);
-                    if (data.length >= 10000) {
-                        dbRequest.pause();
-                        removeListeners();
-                        resolve();
-                    }
-                };
-                async function onDone() {
-                    done = true;
-                    removeListeners();
-                    resolve();
-                };
-                async function onError(err: any) {
-                    removeListeners();
-                    reject(err);
-                };
-                dbRequest.on('row', onRow);
-                dbRequest.on('done', onDone);
-                dbRequest.on('error', onError);
-                dbRequest.resume();
-            });
-        }
-        dbRequest.pause();
         await processStream(readStream);
+        await db.close();
     }
 }
